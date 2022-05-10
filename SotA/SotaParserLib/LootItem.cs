fee65b17a261b076577b22f0722bc2c9f301a255
Friedrich Huber <friedrich.huber@gmx.de>
2022-05-10 17:16:40 +0200

    Move ItemValue up to LootItem Class

--- SotA/SotaParserLib/LootItem.cs
+++ SotA/SotaParserLib/LootItem.cs
@@ -6,30 +6,29 @@ namespace SotaLogParser
 {
     public class LootItem : LogItem
     {
-        public LootItem(DateTime time, string path, int lineNumber, string line, string restOfLine, string itemName, string looterName) : base(time, path, lineNumber, line, restOfLine)
+        public LootItem(DateTime time, string path, int lineNumber, string line, string restOfLine, string itemName, string looterName, int itemValue = 0) : base(time, path, lineNumber, line, restOfLine)
         {
             ItemName = itemName;
             LooterName = looterName;
+            ItemValue = itemValue;
         }
 
         public string ItemName { get; }
         public string LooterName { get; }
+        public int ItemValue { get; } = 0;
     }
 
     public class LootRollItem : LootItem
     {
-        public LootRollItem(DateTime time, string path, int lineNumber, string line, string restOfLine, string itemName, string looterName, int itemValue) : base(time, path, lineNumber, line, restOfLine, itemName, looterName)
+        public LootRollItem(DateTime time, string path, int lineNumber, string line, string restOfLine, string itemName, string looterName, int itemValue) : base(time, path, lineNumber, line, restOfLine, itemName, looterName, itemValue)
         {
-            ItemValue = itemValue;
         }
 
         public void AddRoll(string name, int roll)
         {
             Rolls.Add(name, roll);
         }
-
-        public int ItemValue { get; }
-
+        
         public Dictionary<string, int> Rolls { get; } = new Dictionary<string, int>();
     }
 }
